be322d08f063c5d0236fce78bb14f7afc9973662
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-04-12 15:28:23 +0300

    решение дз (кроме пролистывания галлереи)

--- work.js
+++ work.js
@@ -2,8 +2,12 @@ import gallery from './gallery-items.js';
 
 const galleryContainer = document.querySelector('ul.js-gallery');
 const modal = document.querySelector('div.js-lightbox');
+const modalOverlay = document.querySelector('.lightbox__overlay')
+
 const modalImage = document.querySelector('.lightbox__image');
 const modalBtnClose = document.querySelector('button[data-action="close-lightbox"]');
+
+// const modalBtnClose = document.querySelector('.lightbox__button');
 
 
 
@@ -11,7 +15,16 @@ const markup = createGallery();
 
 galleryContainer.insertAdjacentHTML('beforeend', markup);
 
-galleryContainer.addEventListener('click', onImageClick)
+galleryContainer.addEventListener('click', onImageClick);
+
+modalBtnClose.addEventListener('click', closeModal);
+
+modalOverlay.addEventListener('click', closeModal);
+
+window.addEventListener('keydown',closeModelByEsc)
+
+
+
 
 function createGallery() {
     return gallery.map(({preview, original, description}) => {
@@ -41,17 +54,24 @@ function onImageClick(evt) {
 
     addModal(evt, evtUrl)
     
-    modalBtnClose.addEventListener('click', console.log('1111'));
+    
 }
 
 function addModal(evt,url) {
     evt.preventDefault();
     modal.classList.add('is-open');
     modalImage.setAttribute('src', url);
+    
 }
 
-// function closeModal() {
-//     console.log('1111111');
-//     modal.classList.remove(is-open);
-// }
+function closeModal() {
+    modal.classList.remove('is-open');
+    modalImage.removeAttribute('src');
+}
+
+function closeModelByEsc(evt) {
+    if (evt.keyCode === 27) {
+        closeModal();
+    }
+}
 
