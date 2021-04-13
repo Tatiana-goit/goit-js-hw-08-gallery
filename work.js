4f4c9aa7b5cdab801c874f4903a045ceaad1a4ee
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-04-14 02:03:13 +0300

    Работа с доп заданием

--- work.js
+++ work.js
@@ -3,12 +3,10 @@ import gallery from './gallery-items.js';
 const galleryContainer = document.querySelector('ul.js-gallery');
 const modal = document.querySelector('div.js-lightbox');
 const modalOverlay = document.querySelector('.lightbox__overlay')
-
 const modalImage = document.querySelector('.lightbox__image');
 const modalBtnClose = document.querySelector('button[data-action="close-lightbox"]');
 
-// const modalBtnClose = document.querySelector('.lightbox__button');
-
+const modalContent = document.querySelector('.lightbox__content');
 
 
 const markup = createGallery();
@@ -21,9 +19,9 @@ modalBtnClose.addEventListener('click', closeModal);
 
 modalOverlay.addEventListener('click', closeModal);
 
-window.addEventListener('keydown',closeModelByEsc)
+document.addEventListener('keydown', closeModalByEsc);
 
-
+document.addEventListener('keydown', changeImage)
 
 
 function createGallery() {
@@ -52,16 +50,24 @@ function onImageClick(evt) {
 
     const evtUrl = evt.target.getAttribute('data-source');
 
-    addModal(evt, evtUrl)
-    
-    
+    addModal(evt, evtUrl);
+    // console.log(evt);
+    // console.log(evt.target);
+
+    // console.log(evt.target.parentNode.parentNode);
+    // console.log(this.index);
+    // console.log(this.children);
+    // console.log(this.indexOf);
+
 }
 
 function addModal(evt,url) {
     evt.preventDefault();
     modal.classList.add('is-open');
     modalImage.setAttribute('src', url);
-    
+    // ?!!!!!!!!!!!!!!!!!!
+
+
 }
 
 function closeModal() {
@@ -69,9 +75,45 @@ function closeModal() {
     modalImage.removeAttribute('src');
 }
 
-function closeModelByEsc(evt) {
-    if (evt.keyCode === 27) {
+function closeModalByEsc(evt) {
+    if ((evt.keyCode === 27) & (modal.classList.contains('is-open'))) {
         closeModal();
     }
 }
 
+
+function changeImage(evt) {
+    if ((evt.code === 'ArrowRight') & (evt.code === 'ArrowLeft')){
+        return
+    }
+
+    if (evt.code === 'ArrowRight') {
+        // const allSrc = gallery.map(({ original }) => original);
+        const allSrc = gallery.map(({ li }) => li);
+
+        let currentSrc = allSrc.indexOf(modalImage.src);
+        
+        console.log(currentSrc);
+      
+
+
+        modalImage.src = allSrc[currentSrc + 1];
+        // if (currentSrc=8)
+
+    }
+    if (evt.code === 'ArrowLeft') {
+        const allSrc = gallery.map(({ original }) => original);
+        let currentSrc = allSrc.indexOf(modalImage.src);
+        
+        modalImage.src = allSrc[currentSrc-1]
+    }
+
+}
+
+
+
+// window.addEventListener('click', function(e) {
+//   const node = e.target.parentNode;
+
+//   console.log([...node.children].indexOf(e.target));
+// });
